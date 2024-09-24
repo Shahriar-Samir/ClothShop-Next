e8ad1693085d7a4fa427f85f46898f62100199d2
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-24 21:08:29 +0600

    add: total price details

--- src/app/(Pages)/cart/page.jsx
+++ src/app/(Pages)/cart/page.jsx
@@ -4,8 +4,8 @@ const Cart = () => {
     return (
         <main className='w-full  max-w-[1200px] mx-auto'>
             <h1 className='text-center mt-10 text-3xl font-semibold'>Your Cart</h1>
-            <section className='flex w-full gap-5 mt-10'>
-            <section className='flex flex-col gap-5'>
+            <section className='flex w-full gap-10 justify-between mt-10'>
+            <section className='flex flex-col gap-5 '>
             {[1,2,3,4].map(item=>{
               return <article key={item} className="card lg:card-side bg-base-100 shadow-xl h-[200px]">
               <figure>
@@ -23,8 +23,27 @@ const Cart = () => {
             </article>
             })}
             </section>
-            <aside>
-              
+            <aside className='w-1/4'>
+            <div className="w-full border py-3">
+              <h1 className='text-center text-lg font-semibold'>Price Details</h1>
+  <table className="table w-full mt-2">
+      {/* row 1 */}
+      <tr >
+        <td>Subtotal price</td>
+        <td className='text-end'>1000</td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <td>Discount</td>
+        <td className='text-end'>0</td>
+      </tr>
+      {/* row 3 */}
+      <tr className='border-t '>
+        <td className='font-bold'>Total Price</td>
+        <td className='text-end font-bold'>1000</td>
+      </tr>
+  </table>
+</div>
             </aside>
             </section>
         </main>
