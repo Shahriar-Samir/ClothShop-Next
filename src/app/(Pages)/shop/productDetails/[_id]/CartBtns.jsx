df8752d8a91ead292ad277b80dfc8618a34d0810
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-27 23:08:38 +0600

    add: remove from cart functionaltiy

--- src/app/(Pages)/shop/productDetails/[_id]/CartBtns.jsx
+++ src/app/(Pages)/shop/productDetails/[_id]/CartBtns.jsx
@@ -36,22 +36,27 @@ const CartBtns = ({productData}) => {
         }
     }
 
-    useEffect(()=>{
-        console.log()
-    },[cartItems])
 
-    const removeItem = ()=>[
-
-    ]
-
+    const removeItem = ()=>{
+       if(productDataArray.length>0 && itemsCount>0){
+        const [cartProData] = productDataArray
+        cartProData.amount-=1
+        setItemsCount(itemsCount-1)
+        const newCart = cartItems.filter(item=>{
+          return item.productName === cartProData.name
+        })
+        return localStorage.setItem('cart',JSON.stringify([...newCart,cartProData]))
+       }
+    }
+    
     return (
         <div className="card-actions gap-2 flex-col">
       <div className='flex items-center gap-5 border p-2'>
       <button className='bg-gray-100 h-[40px] w-[40px] rounded-full' onClick={addItem}>+</button>
       <p>{itemsCount}</p>
-      <button className='bg-gray-100 h-[40px] w-[40px] rounded-full'>-</button>
+      <button onClick={removeItem} className='bg-gray-100 h-[40px] w-[40px] rounded-full'>-</button>
       </div>
-      <button className="btn bg-[#DC2626] text-white hover:bg-[#8f1919] w-[300px]">Add to Cart</button>
+      <button className="btn bg-[#DC2626] text-white hover:bg-[#8f1919] w-[300px]" >Add to Cart</button>
     </div>
     );
 };
