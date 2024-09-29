9e354b7fc93a93a14e173a722d6e16a78b95d62f
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-29 15:12:59 +0600

    add: reponsiveness to cart page

--- src/app/(Pages)/payment/success/page.jsx
+++ src/app/(Pages)/payment/success/page.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const page = () => {
     return (
         <div className='w-full h-[80vh] flex justify-center items-center gap-5 flex-col'>
-            <h1 className='text-2xl font-semibold'>Your payment was successful</h1>
+            <h1 className='text-2xl font-semibold text-center'>Your payment was successful</h1>
             <Link href='/' className='btn'><button>Go to Homepage</button></Link>
         </div>
     );
