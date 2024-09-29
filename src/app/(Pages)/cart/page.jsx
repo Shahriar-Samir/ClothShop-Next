1110b553f34e0017339db830f2bfe3b0f267ad19
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-29 17:53:25 +0600

    fix: cart page height

--- src/app/(Pages)/cart/page.jsx
+++ src/app/(Pages)/cart/page.jsx
@@ -6,7 +6,7 @@ const Cart = async () => {
     const session = await getServerSession(authOptions)
     const uid = session?.user?.uid
     return (
-        <main className='w-full  max-w-[1200px] mx-auto'>
+        <main className='w-full  max-w-[1200px] mx-auto min-h-[70vh]'>
             <h1 className='text-center mt-10 text-3xl font-semibold'>Your Cart</h1>
             <section className='flex flex-col-reverse md:flex-row w-full gap-10 justify-center mt-10'>
                 <CartData uid={uid}/>
