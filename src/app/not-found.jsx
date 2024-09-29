1de322fd16ef2346729e3e73610876d9d2ed800e
Shahriar-Samir <shabusiness035@gmail.com>
2024-09-29 17:41:28 +0600

    update: not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className='h-[80vh] gap-4 text-center flex justify-center items-center flex-col '>
       <h1 className='text-2xl font-semibold'>404 - Page Not Found</h1>
-      <p>Oops! The page you're looking for doesn't exist.</p>
+      <p>Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
         <Link href='/' className='btn bg-blue-500 hover:bg-blue-600 text-white'>Go back to Home</Link>
     </div>
   );
